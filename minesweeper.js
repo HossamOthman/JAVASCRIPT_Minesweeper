e24ecbd5f226502bd207c660d348672cda1b57db
Hossam Othman <rowheadrex@gmail.com>
2023-09-01 17:35:40 +0200

    mines are laid

--- minesweeper.js
+++ minesweeper.js
@@ -10,6 +10,8 @@ const TILE_STATUSES = {
 
 export function createBoard(boardSize, numberOfMines) {
     const board = [];
+    const minePositions = getMinePositions(boardSize, numberOfMines);
+    console.log(minePositions)
     for (let x = 0; x < boardSize; x++) {
         const row = [];
         for (let y = 0; y < boardSize; y++) {
@@ -20,6 +22,7 @@ export function createBoard(boardSize, numberOfMines) {
                 element,
                 x,
                 y,
+                mine: minePositions.some(positionMatch.bind(null, {x,y })),
                 get status() {
                     return this.element.dataset.status;
                 },
@@ -34,3 +37,27 @@ export function createBoard(boardSize, numberOfMines) {
     return board;
 };
 
+function getMinePositions(boardSize, numberOfMines){
+    const positions = [];
+
+   while (positions.length < numberOfMines) {
+    const position = {
+        x: randomNumber(boardSize),
+        y: randomNumber(boardSize)
+    }
+
+    if (!positions.some(positionMatch.bind(null, position))) {
+        positions.push(position)
+    }
+   }
+
+    return positions;
+};
+
+function positionMatch(a , b) {
+    return a.x === b.x && a.y === b.y
+}
+
+function randomNumber(size) {
+    return Math.floor(Math.random() * size);
+}
