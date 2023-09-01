6b0466e7f73ddc62692d6986e1373e1e42fe748b
Hossam Othman <rowheadrex@gmail.com>
2023-09-01 21:25:31 +0200

    Final, Win lose Conditions

--- script.js
+++ script.js
@@ -1,49 +1,96 @@
 // Display / UI
-import { TILE_STATUSES, createBoard, markTile, revealTile } from './minesweeper.js';
+import {
+  TILE_STATUSES,
+  createBoard,
+  markTile,
+  revealTile,
+} from "./minesweeper.js";
 
 const BOARD_SIZE = 10;
 const NUMBER_OF_MINES = 10;
 
 // populate the board with tiles/mines
 const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES);
-const boardElement = document.querySelector('.board');
-const minesLeftText = document.querySelector('[data-mine-count]');
+const boardElement = document.querySelector(".board");
+const minesLeftText = document.querySelector("[data-mine-count]");
+const messageText = document.querySelector(".subtext");
 
 // console.log(board);
 
-board.forEach(row => {
-    row.forEach(tile => {
-        boardElement.append(tile.element);
-        tile.element.addEventListener('click', () => {
-            revealTile(board, tile);
-        })
-        tile.element.addEventListener('contextmenu', e => {
-            e.preventDefault();
-            markTile(tile);
-            listMinesLeft();
-        });
+board.forEach((row) => {
+  row.forEach((tile) => {
+    boardElement.append(tile.element);
+    // left click on tiles >> reveal tiles
+    tile.element.addEventListener("click", () => {
+      revealTile(board, tile);
+      checkGameEnd();
     });
+    // right click on tiles >> mark tiles
+    tile.element.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      markTile(tile);
+      listMinesLeft();
+    });
+  });
 });
-boardElement.style.setProperty('--size', BOARD_SIZE);
+boardElement.style.setProperty("--size", BOARD_SIZE);
 minesLeftText.textContent = NUMBER_OF_MINES;
 
 function listMinesLeft() {
-    const markedTilesCount = board.reduce((count, row) => {
-        return count + row.filter(tile => tile.status === TILE_STATUSES.MARKED).length
-    }, 0);
+  const markedTilesCount = board.reduce((count, row) => {
+    return (
+      count + row.filter((tile) => tile.status === TILE_STATUSES.MARKED).length
+    );
+  }, 0);
 
-    minesLeftText.textContent = NUMBER_OF_MINES - markedTilesCount
+  minesLeftText.textContent = NUMBER_OF_MINES - markedTilesCount;
 }
 
-
-
-
-// left click on tiles >> reveal tiles
-
-
-// right click on tiles >> mark tiles
-
-
 // check for win/lose
+function checkGameEnd() {
+  const win = checkWin(board);
+  const lose = checkLose(board);
 
+  if (win || lose) {
+    boardElement.addEventListener("click", stopProp, { capture: true }); // capture fraze is to give priority to this click event over the above
+    boardElement.addEventListener("contextmenu", stopProp, { capture: true });
+  }
 
+  if (win) {
+    messageText.textContent = "you won!";
+  }
+  if (lose) {
+    messageText.textContent = "you lost";
+    board.forEach((row) => {
+      row.forEach((tile) => {
+        if (tile.status === TILE_STATUSES.MARKED) markTile(tile);
+        if (tile.mine) revealTile(board, tile);
+      });
+    });
+  }
+}
+
+function checkWin(board) {
+  return board.every((row) => {
+    return row.every((tile) => {
+      return (
+        tile.status === TILE_STATUSES.NUMBER ||
+        (tile.mine &&
+          (tile.status === TILE_STATUSES.HIDDEN ||
+            tile.status === TILE_STATUSES.MARKED))
+      );
+    });
+  });
+}
+
+function checkLose(board) {
+  return board.some((row) => {
+    return row.some((tile) => {
+      return tile.status === TILE_STATUSES.MINE;
+    });
+  });
+}
+
+function stopProp(e) {
+  e.stopImmediatePropagation();
+}
