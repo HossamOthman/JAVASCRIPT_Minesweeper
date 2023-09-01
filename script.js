110bd477334b61701502d2731e6caf04ac66b46d
Hossam Othman <rowheadrex@gmail.com>
2023-09-01 20:15:40 +0200

    number of Tiles count

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Display / UI
-import { createBoard, markTile } from './minesweeper.js';
+import { TILE_STATUSES, createBoard, markTile } from './minesweeper.js';
 
 const BOARD_SIZE = 10;
-const NUMBER_OF_MINES = 2;
+const NUMBER_OF_MINES = 10;
 
 // populate the board with tiles/mines
 const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES);
 const boardElement = document.querySelector('.board');
 const minesLeftText = document.querySelector('[data-mine-count]');
-console.log(board);
 
+// console.log(board);
 
 board.forEach(row => {
     row.forEach(tile => {
@@ -20,11 +20,24 @@ board.forEach(row => {
         tile.element.addEventListener('contextmenu', e => {
             e.preventDefault();
             markTile(tile);
+            listMinesLeft();
         });
     });
 });
-boardElement.style.setProperty('--size', BOARD_SIZE)
-minesLeftText.textContent = NUMBER_OF_MINES
+boardElement.style.setProperty('--size', BOARD_SIZE);
+minesLeftText.textContent = NUMBER_OF_MINES;
+
+function listMinesLeft() {
+    const markedTilesCount = board.reduce((count, row) => {
+        return count + row.filter(tile => tile.status === TILE_STATUSES.MARKED).length
+    }, 0);
+
+    minesLeftText.textContent = NUMBER_OF_MINES - markedTilesCount
+}
+
+
+
+
 // left click on tiles >> reveal tiles
 
 
